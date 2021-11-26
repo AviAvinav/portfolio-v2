45ed5ebdb4f00559956f086459fecf5648926b01
Avi Avinav <aviavinav2004@gmail.com>
2021-11-26 21:01:59 +0530

    Add: Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import Navbar from "../components/Navbar";
+
 const Home = () => {
   return (
     <div className="font-outfit">
@@ -8,7 +10,9 @@ const Home = () => {
         <meta name="description" content="Avi Avinav's portfolio website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-full w-full bg-gray-50 dark:bg-gray-800"></div>
+      <div className="h-full w-full bg-gray-50 dark:bg-gray-800">
+        <Navbar />
+      </div>
     </div>
   );
 };
