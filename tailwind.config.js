d901250f0936d8e31f40d55e0c84389619859bbf
Avi Avinav <aviavinav2004@gmail.com>
2022-03-10 00:24:27 +0530

    migrate to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         outfit: ["Outfit", "sans-serif"],
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
